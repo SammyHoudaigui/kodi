12b41f9c51255e7132212e84ab51af50f9687404
Sammy Houdaigui <sahoudaigui@gmail.com>
2016-08-29 10:39:12 +0100

    Initial Commit

---
+++ kodi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  kodi
+//
+//  Created by Sammy Houdaigui on 8/29/16.
+//  Copyright © 2016 Sammy Houdaigui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
